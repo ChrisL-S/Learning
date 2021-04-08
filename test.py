3b4753f5c053c2940977bcd5705c78196a86a221
Lester-Smith <chris.lester-smith@metoffice.gov.uk>
2021-04-08 12:20:02 +0100

    added test.py

--- test.py
+++ test.py
@@ -1,2 +1,22 @@
-legend
-legend again
+import unittest
+
+
+def myfunc():
+    return False
+
+
+a = 1
+b = 2
+
+
+class testtruth(unittest.TestCase):
+
+    def test_myfunc(self):
+        self.assertFalse(myfunc())
+
+    def test(self):
+        self.assertEqual(a, b)
+
+
+if __name__ == '__main__':
+    unittest.main()
